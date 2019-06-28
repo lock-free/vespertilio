5e8fb4b514620e0f9cea6dd27675397e32000515
loveKino <842914439@qq.com>
2019-06-28 16:51:27 +0800

    opt

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ const {
 } = require('flexdeploy/src/util');
 
 const deployDpm = async (dpmDeployCnfPath, onlineType) => {
-  await spawnp('../node_modules/.bin/ideploy', [
-    '--config',
-    dpmDeployCnfPath,
-    '--onlineType',
-    onlineType
-  ], {
-    cwd: __dirname,
-    stdio: 'inherit'
-  });
+  await spawnp(
+    '../node_modules/.bin/ideploy',
+    //'../../insight-in-one/thirdparty/flexdeploy/bin/ideploy',
+
+    [
+      '--config',
+      dpmDeployCnfPath,
+      '--onlineType',
+      onlineType
+    ], {
+      cwd: __dirname,
+      stdio: 'inherit'
+    });
 };
 
 module.exports = {
