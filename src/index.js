48cf339e5bf158d4ec578a2cee041d18f321ea87
loveKino <842914439@qq.com>
2019-08-05 14:13:22 +0800

    adjust config object structure

--- src/index.js
+++ src/index.js
@@ -80,22 +80,19 @@ const getVespertilioConf = async ({
 
   // merge with default configuration options
   _.merge(cnf, {
-    dpm: {
-      'deploy-cnf': `${baseDir}/dpm/deploy-cnf.json`,
-      'src': `${baseDir}/dpm/src`,
-      'cnfDir': `${configDir}/dpm/cnf`,
-      'cnfFile': `${configDir}/dpm/config.json`,
-      'data': `${configDir}/data`,
-      deploy: cnf.machine.dpm
-    },
-    httpna: {
-      'cnfDir': `${configDir}/http_na`
-    },
-    source: {
+    build: {
       'repoRoot': `${baseDir}/code_repo`,
+      dpm: {
+        'deploy-cnf': `${baseDir}/dpm/deploy-cnf.json`,
+        'src': `${baseDir}/dpm/src`,
+        'cnfDir': `${configDir}/dpm/cnf`,
+        'cnfFile': `${configDir}/dpm/config.json`,
+        'data': `${configDir}/data`,
+        deploy: cnf.machine.dpm
+      }
     },
-    remote: {
-      NAs: _.flatten(cnf.remote.naPorts.map((port) => {
+    deploy: {
+      NAs: _.flatten(cnf.deploy.naPorts.map((port) => {
         return cnf.machine.na.instances.staging.map(({
           host
         }) => {
@@ -118,20 +115,25 @@ const getVespertilioConf = async ({
     }
   });
 
-  // add automatic name for worker
-  cnf.remote.worker.Workers = cnf.remote.worker.Workers.map((worker) => {
-    worker.name = `${cnf.name}_${worker.ServiceType}`;
+  // add automatic name and serviceType for deploy  worker
+  cnf.deploy.workers = _.map(cnf.deploy.workers, (worker, serviceType) => {
+    worker.serviceType = serviceType;
+    worker.name = `${cnf.name}_${worker.serviceType}`;
+    return worker;
+  });
+
+  cnf.build.workers = _.map(cnf.build.workers, (worker, serviceType) => {
+    worker.serviceType = serviceType;
     return worker;
   });
 
   // resolve paths
-  cnf.source.repoRoot = path.resolve(cnfDir, cnf.source.repoRoot);
+  cnf.build.repoRoot = path.resolve(cnfDir, cnf.build.repoRoot);
 
-  cnf.dpm.cnfDir = path.join(cnfDir, cnf.dpm.cnfDir);
-  cnf.dpm.cnfFile = path.join(cnfDir, cnf.dpm.cnfFile);
-  cnf.dpm.src = path.resolve(cnfDir, cnf.dpm.src);
-  cnf.dpm['deploy-cnf'] = path.resolve(cnfDir, cnf.dpm['deploy-cnf']);
-  cnf.httpna.cnfDir = path.join(cnfDir, cnf.httpna.cnfDir);
+  cnf.build.dpm.cnfDir = path.join(cnfDir, cnf.build.dpm.cnfDir);
+  cnf.build.dpm.cnfFile = path.join(cnfDir, cnf.build.dpm.cnfFile);
+  cnf.build.dpm.src = path.resolve(cnfDir, cnf.build.dpm.src);
+  cnf.build.dpm['deploy-cnf'] = path.resolve(cnfDir, cnf.build.dpm['deploy-cnf']);
 
   cnf.privateKeyDir = path.resolve(cnfDir, cnf.privateKeyDir);
 
@@ -142,16 +144,16 @@ const syncBaseProjects = async (cnf, {
   upd
 }) => {
   // if need to force update demo project,
-  if (!await existsDir(cnf.dpm.src) || upd) {
-    await del([cnf.dpm.src]);
-    await spawnp('git', ['clone', 'git@github.com:lock-free/dpm-cluster-demo.git', cnf.dpm.src]);
-    await del([path.join(cnf.dpm.src, '.git')]);
+  if (!await existsDir(cnf.build.dpm.src) || upd) {
+    await del([cnf.build.dpm.src]);
+    await spawnp('git', ['clone', 'git@github.com:lock-free/dpm-cluster-demo.git', cnf.build.dpm.src]);
+    await del([path.join(cnf.build.dpm.src, '.git')]);
   }
 
   await Promise.all([
-    updateRepo('git@github.com:lock-free/dpm_service.git', cnf.source.repoRoot, 'dpm_service'),
-    updateRepo('git@github.com:lock-free/na_service.git', cnf.source.repoRoot, 'na_service'),
-    updateRepo('git@github.com:lock-free/httpna_service.git', cnf.source.repoRoot, 'httpna_service')
+    updateRepo('git@github.com:lock-free/dpm_service.git', cnf.build.repoRoot, 'dpm_service'),
+    updateRepo('git@github.com:lock-free/na_service.git', cnf.build.repoRoot, 'na_service'),
+    updateRepo('git@github.com:lock-free/httpna_service.git', cnf.build.repoRoot, 'httpna_service')
   ]);
 };
 
@@ -167,9 +169,9 @@ const updateRepo = async (gitAddr, repoRoot, targetDir) => {
 };
 
 // copy stage dir and empty Makefile
-const copyStageToDpmSrcRepo = async (repoRoot, srcRepo, targetDir) => {
+const copyStageToDpmSrcRepo = async (repoRoot, srcRepo, targetDir, stageDir = 'stage') => {
   const srcStage = path.resolve(srcRepo, targetDir, 'stage');
-  const repoStage = path.resolve(repoRoot, targetDir, 'stage');
+  const repoStage = path.resolve(repoRoot, targetDir, stageDir);
 
   await copyDir(repoStage, srcStage);
   await spawnp('cp', [path.join(__dirname, '../res/makefile'), path.resolve(srcRepo, targetDir, 'makefile')]);
@@ -177,21 +179,21 @@ const copyStageToDpmSrcRepo = async (repoRoot, srcRepo, targetDir) => {
 
 const getWorkers = (cnf) => {
   return (
-    cnf.only ? cnf.source.workers.filter(({
+    cnf.only ? cnf.build.workers.filter(({
       serviceType
-    }) => serviceType === cnf.only) : cnf.source.workers
+    }) => serviceType === cnf.only) : cnf.build.workers
   );
 };
 
 // copy projects to dpm source dir
 const buildDpm = async (cnf) => {
   // copy binary
-  const dpmBinPath = path.resolve(cnf.dpm.src, './stage/bin/');
+  const dpmBinPath = path.resolve(cnf.build.dpm.src, './stage/bin/');
   await mkdirp(dpmBinPath);
-  await spawnp('cp', [path.resolve(cnf.source.repoRoot, 'dpm_service/stage/bin/dpm_service'), dpmBinPath]);
+  await spawnp('cp', [path.resolve(cnf.build.repoRoot, 'dpm_service/stage/bin/dpm_service'), dpmBinPath]);
 
   // write docker-compose.yml
-  await writeTxt(path.resolve(cnf.dpm.src, 'docker-compose.yml'), `
+  await writeTxt(path.resolve(cnf.build.dpm.src, 'docker-compose.yml'), `
 version: '3'
 services:
   ${cnf.name}_dpm_service:
@@ -202,7 +204,7 @@ services:
   `);
 
   // copy deploy-cnf.json to dpm dir
-  await writeJson(cnf.dpm['deploy-cnf'],
+  await writeJson(cnf.build.dpm['deploy-cnf'],
     Object.assign({
       'project': cnf.name || 'vespertilio-cluster',
 
@@ -214,7 +216,7 @@ services:
         'pre': [],
         'afterDeployRemoteCmds': []
       }
-    }, cnf.dpm.deploy)
+    }, cnf.build.dpm.deploy)
   );
 };
 
@@ -228,25 +230,25 @@ const build = async (cnf, {
 
   await copySourceCodeToDpmSrc(cnf);
 
-  const srcRepo = path.resolve(cnf.dpm.src, './stage/data/src');
+  const srcRepo = path.resolve(cnf.build.dpm.src, './stage/data/src');
 
   await Promise.all([
     await copyDpmData(cnf),
-    // copy config for httpna
-    await copyDir(cnf.httpna.cnfDir, path.join(cnf.dpm.src, './stage/data/src/httpna_service/stage/data')),
   ].concat(
     // copy workers data
     getWorkers(cnf).map(async ({
       serviceType
     }) => {
-      // copy data dir
-      if (cnf.dpm.data) {
-        const workerDataSrcDir = path.join(cnf.cwd, cnf.dpm.data, serviceType);
-        const workerDataTarDir = path.join(srcRepo, serviceType, './stage/data');
-        if (await existsDir(workerDataSrcDir)) {
-          await copyDir(workerDataSrcDir, workerDataTarDir);
-        }
+      // get deploy  worker
+      const worker = _.find(cnf.deploy.workers, (worker) => serviceType === worker.serviceType);
+      if (!worker) {
+        throw new Error(`missing deploy worker configuration for worker ${serviceType}`);
       }
+
+      // write data files
+      await Promise.all(_.map(worker.data, (value, filename) => {
+        return writeJson(path.join(srcRepo, serviceType, `./stage/data/${filename}`), value);
+      }));
     })
   ));
 
@@ -255,17 +257,18 @@ const build = async (cnf, {
 
 // copy code to dpm src dir
 const copySourceCodeToDpmSrc = async (cnf) => {
-  const srcRepo = path.resolve(cnf.dpm.src, './stage/data/src');
+  const srcRepo = path.resolve(cnf.build.dpm.src, './stage/data/src');
 
   await Promise.all([
     // copy basic services
-    await copyStageToDpmSrcRepo(cnf.source.repoRoot, srcRepo, 'na_service'),
-    await copyStageToDpmSrcRepo(cnf.source.repoRoot, srcRepo, 'httpna_service')
+    await copyStageToDpmSrcRepo(cnf.build.repoRoot, srcRepo, 'na_service'),
+    await copyStageToDpmSrcRepo(cnf.build.repoRoot, srcRepo, 'httpna_service')
   ].concat(
     // copy workers
     getWorkers(cnf).map(async ({
       serviceType,
-      buildCmd
+      buildCmd,
+      stageDir
     }) => {
       // build first
       if (buildCmd) {
@@ -274,56 +277,68 @@ const copySourceCodeToDpmSrc = async (cnf) => {
         });
       }
       // copy source from repo root to dpm src repo
-      await copyStageToDpmSrcRepo(cnf.source.repoRoot, srcRepo, serviceType);
+      await copyStageToDpmSrcRepo(cnf.build.repoRoot, srcRepo, serviceType, stageDir);
     })));
 };
 
 const copyDpmData = async (cnf) => {
   // copy common dir
-  if (await existsDir(path.join(cnf.dpm.cnfDir, 'common'))) {
-    await copyDir(path.join(cnf.dpm.cnfDir, 'common'), path.join(cnf.dpm.src, './stage/data/cnf/common'));
+  if (await existsDir(path.join(cnf.build.dpm.cnfDir, 'common'))) {
+    await copyDir(path.join(cnf.build.dpm.cnfDir, 'common'), path.join(cnf.build.dpm.src, './stage/data/cnf/common'));
   }
 
   // copy private dir
-  if (await existsDir(path.join(cnf.dpm.cnfDir, 'private'))) {
-    await copyDir(path.join(cnf.dpm.cnfDir, 'private'), path.join(cnf.dpm.src, './stage/data/cnf/private'));
+  if (await existsDir(path.join(cnf.build.dpm.cnfDir, 'private'))) {
+    await copyDir(path.join(cnf.build.dpm.cnfDir, 'private'), path.join(cnf.build.dpm.src, './stage/data/cnf/private'));
   }
 
   // copy dpm cnf na.json on the fly
-  await writeJson(path.join(cnf.dpm.src, './stage/data/cnf/na.json'), {
+  await writeJson(path.join(cnf.build.dpm.src, './stage/data/cnf/na.json'), {
     'NADeployCnfPath': '/data/cnf/common/na/deploy-cnf.json',
     'NAMachineCnfPath': '/data/cnf/private/machine/na.json',
-    NAs: cnf.remote.NAs
+    NAs: cnf.deploy.NAs
   });
 
   // copy dpm worker.json on the fly
-  await writeJson(path.join(cnf.dpm.src, './stage/data/cnf/worker.json'), {
+  await writeJson(path.join(cnf.build.dpm.src, './stage/data/cnf/worker.json'), {
     'WorkerDeployCnfPath': '/data/cnf/common/worker/deploy-cnf.json',
     'WorkerMachineCnfPath': '/data/cnf/private/machine/worker.json',
-    Workers: cnf.remote.worker.Workers,
-    Machines: cnf.remote.worker.Machines
+    Workers: cnf.deploy.workers.map(({
+      name,
+      serviceType,
+      dcyTplPath,
+      dcyTplConfigPath
+    }) => {
+      return _.pickBy({
+        name,
+        ServiceType: serviceType,
+        DcyTplPath: dcyTplPath,
+        DcyTplConfigPath: dcyTplConfigPath
+      }, v => v !== undefined);
+    }),
+    Machines: cnf.deploy.worker.Machines
   });
 
   // copy machine/na.json on the fly
-  await writeJson(path.join(cnf.dpm.src, './stage/data/cnf/private/machine/na.json'),
+  await writeJson(path.join(cnf.build.dpm.src, './stage/data/cnf/private/machine/na.json'),
     cnf.machine.na
   );
 
   // copy machine/worker.json on the fly
-  await writeJson(path.join(cnf.dpm.src, './stage/data/cnf/private/machine/worker.json'),
+  await writeJson(path.join(cnf.build.dpm.src, './stage/data/cnf/private/machine/worker.json'),
     cnf.machine.na
   );
 
   // copy id_rsa
   await copyDir(cnf.privateKeyDir,
-    path.join(cnf.dpm.src, './stage/data/cnf/private/ssh'));
+    path.join(cnf.build.dpm.src, './stage/data/cnf/private/ssh'));
 
   // copy dpm config.json on the fly
-  await writeJson(path.join(cnf.dpm.src, './stage/data/config.json'),
+  await writeJson(path.join(cnf.build.dpm.src, './stage/data/config.json'),
     // default dpm config.json
     {
       Only: cnf.only || '',
-      RemoteRoot: cnf.remote.root,
+      RemoteRoot: cnf.deploy.root,
 
       'OnlineType': 'staging',
       'NAConfPath': '/data/cnf/na.json',
@@ -353,13 +368,13 @@ const run = async (cmd, {
       });
       break;
     case 'deploy':
-      await deployDpm(cnf.dpm['deploy-cnf'], 'staging');
+      await deployDpm(cnf.build.dpm['deploy-cnf'], 'staging');
       break;
     case 'run':
       await build(cnf, {
         upd
       });
-      await deployDpm(cnf.dpm['deploy-cnf'], 'staging');
+      await deployDpm(cnf.build.dpm['deploy-cnf'], 'staging');
       break;
     default:
       throw new Error(`unexpected command: ${cmd}`);
